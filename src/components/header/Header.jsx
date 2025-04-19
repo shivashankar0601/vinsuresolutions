dbb5174787f76642633ecfeb54abe31a9d22ae4d
Shiva Shankar <shivap@retisoft.com>
2025-04-19 14:15:16 -0400

    training menu wip

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import "./Header.css";
 import {
 	Navbar,
 	Typography,
@@ -8,7 +9,6 @@ import {
 	MenuList,
 	MenuItem,
 	Avatar,
-	Card,
 	IconButton,
 	Collapse,
 } from "@material-tailwind/react";
@@ -18,17 +18,13 @@ import {
 	Square3Stack3DIcon,
 	ChevronDownIcon,
 	Cog6ToothIcon,
-	InboxArrowDownIcon,
 	LifebuoyIcon,
 	PowerIcon,
-	RocketLaunchIcon,
 	Bars2Icon,
 	BanknotesIcon,
 	UserGroupIcon,
-	IdentificationIcon,
 	GlobeAltIcon,
 	InformationCircleIcon,
-	ServerStackIcon,
 	BriefcaseIcon,
 } from "@heroicons/react/24/solid";
 
@@ -40,6 +36,44 @@ const profileMenuItems = [
 	{ label: "Sign Out", icon: PowerIcon },
 ];
 
+const navListMenuItems = [
+	{
+		title: "Java",
+		description:
+			"Java is a high-level, class-based, object-oriented programming language.",
+	},
+	{
+		title: "Salesforce Admin",
+		description:
+			"Salesforce Admin is a cloud-based software platform for customer relationship management.",
+	},
+	{
+		title: "Business Systems Analyst",
+		description:
+			"Business Systems Analyst is a professional who analyzes and designs information systems.",
+	},
+	{
+		title: "Quality Analyst",
+		description:
+			"Quality Analyst is a professional who ensures that products meet quality standards.",
+	},
+	{
+		title: "Project Management",
+		description:
+			"Project Management is the process of planning, executing, and closing projects.",
+	},
+];
+
+const navListItems = [
+	{ label: "Payroll", icon: BanknotesIcon },
+	{ label: "Recruitment", icon: UserGroupIcon },
+	{ label: "Training", icon: Square3Stack3DIcon },
+	{ label: "BPO & KPO", icon: GlobeAltIcon },
+	{ label: "About Us", icon: InformationCircleIcon },
+];
+
+const iconHeight = "h-[24px] w-[24px]";
+
 const ProfileMenu = () => {
 	const [isMenuOpen, setIsMenuOpen] = useState(false);
 	const closeMenu = () => setIsMenuOpen(false);
@@ -50,18 +84,18 @@ const ProfileMenu = () => {
 				<Button
 					variant="text"
 					color="blue-gray"
-					className="flex items-center gap-1 rounded-full py-1 px-1.5"
+					className="flex items-center gap-1 rounded-full py-1 px-1.5 bg-white"
 				>
 					<Avatar
 						variant="circular"
 						size="sm"
 						alt="User avatar"
-						className="p-0.5 h-9 w-9"
+						className="h-10 w-10"
 						src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?auto=format&fit=crop&w=1480&q=80"
 					/>
 					<ChevronDownIcon
 						strokeWidth={2.5}
-						className={`text-primary h-3 w-3 transition-transform ${
+						className={`text-primary h-4 w-4 transition-transform ${
 							isMenuOpen ? "rotate-180" : ""
 						}`}
 					/>
@@ -102,43 +136,13 @@ const ProfileMenu = () => {
 	);
 };
 
-const navListMenuItems = [
-	{
-		title: "Java",
-		description:
-			"Java is a high-level, class-based, object-oriented programming language.",
-	},
-	{
-		title: "Salesforce Admin",
-		description:
-			"Salesforce Admin is a cloud-based software platform for customer relationship management.",
-	},
-	{
-		title: "Business Systems Analyst",
-		description:
-			"Business Systems Analyst is a professional who analyzes and designs information systems.",
-	},
-	{
-		title: "Quality Analyst",
-		description:
-			"Quality Analyst is a professional who ensures that products meet quality standards.",
-	},
-	{
-		title: "Project Management",
-		description:
-			"Project Management is the process of planning, executing, and closing projects.",
-	},
-];
-
-const iconHeight = "h-[24px] w-[24px]";
-
 const NavListMenu = () => {
 	const [isMenuOpen, setIsMenuOpen] = useState(false);
 
 	const renderItems = navListMenuItems.map(({ title, description }) => (
 		<a href="#" key={title}>
 			<MenuItem>
-				<Typography variant="h6" color="blue-gray" className="mb-1">
+				<Typography variant="h6" className="mb-1">
 					{title}
 				</Typography>
 				<Typography
@@ -162,9 +166,9 @@ const NavListMenu = () => {
 						variant="small"
 						className="font-normal"
 					>
-						<MenuItem className="hidden items-center gap-2 font-medium text-blue-gray-900 lg:flex lg:rounded-full">
+						<MenuItem className="hidden items-center gap-2 font-medium  lg:flex lg:rounded-full">
 							<Square3Stack3DIcon className={iconHeight} />
-							<span className="font-bold text-lg">Training</span>
+							<span className="text-lg">Training</span>
 							<ChevronDownIcon
 								strokeWidth={2}
 								className={`h-4 w-4 mt-[5px] transition-transform ${
@@ -174,37 +178,47 @@ const NavListMenu = () => {
 						</MenuItem>
 					</Typography>
 				</MenuHandler>
-				<MenuList className="hidden w-[40rem] grid-cols-1 overflow-visible lg:grid border-0 rounded-lg shadow-lg bg-white p-4">
-					<ul className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-1 gap-4 w-full hover:!border-0">
-						{navListMenuItems.map(
-							({ title, description, icon: Icon }) => (
-								<li key={title} className="relative group">
-									<a
-										href="#"
-										className="flex items-center gap-3 bg-gray-100 hover:bg-gray-200 transition-all p-4 rounded-lg"
-									>
-										{Icon && (
-											<div className="bg-blue-100 text-blue-600 p-2 rounded-lg">
-												<Icon className="h-5 w-5" />
-											</div>
-										)}
-										<span className="font-medium text-blue-gray-800">
-											{title}
-										</span>
-									</a>
+				{/* <MenuList className="hidden grid-cols-1 overflow-visible lg:grid border-0 rounded-lg shadow-lg bg-white p-2">
+					<ul className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-1 gap-4 hover:!border-0">
+						{navListMenuItems.map(({ title, description }) => (
+							<li key={title} className="relative group">
+								<a
+									href="#"
+									className="flex items-center gap-3 hover:bg-gray-100 transition-all p-1 rounded-lg"
+								>
+									<span className="font-medium text-primary text-lg">
+										{title}
+									</span>
+								</a>
 
-									{/* Tooltip */}
-									<div className="absolute z-10 hidden group-hover:block bg-white text-sm text-gray-700 shadow-md rounded p-3 w-64 -bottom-24 left-0">
-										{description}
-									</div>
-								</li>
-							)
-						)}
+								<div className="absolute z-10 hidden group-hover:block bg-white text-sm text-primary shadow-md rounded p-3 w-64 -bottom-24 left-0">
+									{description}
+								</div>
+							</li>
+						))}
 					</ul>
+				</MenuList> */}
+
+				<MenuList className="p-1 focus:outline-none border-0">
+					{navListMenuItems.map(({ title, description }) => (
+						<MenuItem
+							key={title}
+							className={`flex items-center gap-2 rounded hover:bg-gray-100`}
+						>
+							<Typography
+								as="span"
+								variant="small"
+								className="font-normal"
+								color="inherit"
+							>
+								{title}
+							</Typography>
+						</MenuItem>
+					))}
 				</MenuList>
 			</Menu>
 
-			<MenuItem className="flex items-center gap-2 font-medium text-blue-gray-900 lg:hidden">
+			<MenuItem className="flex items-center gap-2 font-medium  lg:hidden">
 				<Square3Stack3DIcon className={iconHeight} />
 				Pages
 			</MenuItem>
@@ -216,33 +230,28 @@ const NavListMenu = () => {
 	);
 };
 
-const navListItems = [
-	{ label: "Payroll", icon: BanknotesIcon },
-	{ label: "Recruitment", icon: UserGroupIcon },
-	{ label: "Training", icon: Square3Stack3DIcon },
-	{ label: "BPO & KPO", icon: GlobeAltIcon },
-	{ label: "About Us", icon: InformationCircleIcon },
-];
-
 const NavList = () => (
 	<ul className="mt-2 mb-4 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center">
-		<NavListMenu />
-		{navListItems.map(({ label, icon }) => (
-			<Typography
-				key={label}
-				as="a"
-				href="#"
-				variant="small"
-				className="font-medium"
-			>
-				<MenuItem className="flex items-center gap-x-2 lg:rounded-full">
-					{React.createElement(icon, {
-						className: "h-[24px] w-[24px]",
-					})}
-					<span className="font-bold text-lg">{label}</span>
-				</MenuItem>
-			</Typography>
-		))}
+		{navListItems.map(({ label, icon }) =>
+			label === "Training" ? (
+				<NavListMenu />
+			) : (
+				<Typography
+					key={label}
+					as="a"
+					href="#"
+					variant="small"
+					className="font-medium"
+				>
+					<MenuItem className="flex items-center gap-x-2 lg:rounded-full">
+						{React.createElement(icon, {
+							className: "h-[24px] w-[24px]",
+						})}
+						<span className="text-lg">{label}</span>
+					</MenuItem>
+				</Typography>
+			)
+		)}
 	</ul>
 );
 
@@ -260,7 +269,7 @@ const Header = () => {
 	}, []);
 
 	return (
-		<Navbar className="vs-nav border-0 border-radius-none py-4 rounded-none">
+		<Navbar className="vs-nav bg-primary text-white border-0 border-radius-none py-4 rounded-none">
 			<div className="relative mx-auto flex items-center justify-between container">
 				<Typography
 					as="a"
@@ -280,13 +289,28 @@ const Header = () => {
 						color="white"
 						variant="text"
 						onClick={toggleIsNavOpen}
-						className="l-0 ml-auto mr-2 lg:hidden bg-primary"
+						className="l-0 ml-auto mr-2 lg:hidden bg-white"
 					>
 						<Bars2Icon className="h-6 w-6" />
 					</IconButton>
 				)}
 
-				<ProfileMenu />
+				{/* <ProfileMenu /> */}
+				<Button
+					variant="text"
+					color="white"
+					className="rounded-full bg-white text-primary btn-primary"
+				>
+					Login
+				</Button>
+				{/* 
+				<Button
+					variant="text"
+					color="white"
+					className="rounded-full bg-primary text-primary btn-secondary"
+				>
+					Sign Up
+				</Button> */}
 			</div>
 
 			<Collapse open={isNavOpen} className="overflow-scroll">
